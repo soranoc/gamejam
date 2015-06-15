7e8a3b5e3a74c0a6ccb4f3faf897392dbfa66b31
Clement SORANO <clement.sorano@etudiant.univ-lille1.fr>
2015-06-15 11:45:09 +0200

    Grille

--- src/grille/Grille.java
+++ src/grille/Grille.java
@@ -6,6 +6,8 @@ public class Grille {
 	private int hauteur = 12;
 	private int largeur = 18;
 	private Cellule[][] grille;
+	private Branche brancheGauche;
+	private Branche brancheDroite;
 
 	public Grille (){
 		for (int i = 0; i < grille.length; i++) {
@@ -13,17 +15,25 @@ public class Grille {
 				grille[i][j] = new Vide();
 			}
 		}
+		brancheDroite = new Branche(30);
+		brancheGauche = new Branche(30);
 		getExemple();
 	}
 	
 	public void setSegment(int x, int y){
-		
+		grille[x][y] = new SegmentBranche(x,y);
 	}
 	
 	private void getExemple(){
-		grille[1][5] = new SegmentBranche(5);
-		grille[1][6] = new SegmentBranche(6);
-		grille[2][7] = new SegmentBranche(7);
-		grille[2][7] = new SegmentBranche(7);
+		SegmentBranche seg1 = new SegmentBranche(1,5);
+		SegmentBranche seg2 = new SegmentBranche(1,6);
+		SegmentBranche seg3 = new SegmentBranche(2,7);
+		SegmentBranche seg4 = new SegmentBranche(3,7);
+		SegmentBranche seg5 = new SegmentBranche(4,8);
+		SegmentBranche seg6 = new SegmentBranche(4,9);
+		SegmentBranche seg7 = new SegmentBranche(5,10);
+		SegmentBranche seg8 = new SegmentBranche(6,9);
+		SegmentBranche seg9 = new SegmentBranche(7,9);
+		
 	}
 }
