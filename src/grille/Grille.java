9da29ae01fa85bb5522c5a903717c807376a7cd4
Clement SORANO <clement.sorano@etudiant.univ-lille1.fr>
2015-06-16 11:17:42 +0200

    lel

--- src/grille/Grille.java
+++ src/grille/Grille.java
@@ -56,17 +56,17 @@ public class Grille {
 	
 	private void getExemple(){
 		// Branche Gauche
-		SegmentBranche seg1 = new SegmentBranche(5,1);
-		SegmentBranche seg2 = new SegmentBranche(6,1);
-		SegmentBranche seg3 = new SegmentBranche(7,2);
-		SegmentBranche seg4 = new SegmentBranche(7,3);
-		SegmentBranche seg5 = new SegmentBranche(8,4);
-		SegmentBranche seg6 = new SegmentBranche(9,4);
-		SegmentBranche seg7 = new SegmentBranche(10,5);
-		SegmentBranche seg8 = new SegmentBranche(9,6);
-		SegmentBranche seg9 = new SegmentBranche(9,7);
-		SegmentBranche seg10 = new SegmentBranche(10,7);
-		SegmentBranche seg11 = new SegmentBranche(11,7);
+		SegmentBranche seg1 = new SegmentBranche(1,5);
+		SegmentBranche seg2 = new SegmentBranche(1,6);
+		SegmentBranche seg3 = new SegmentBranche(2,7);
+		SegmentBranche seg4 = new SegmentBranche(3,7);
+		SegmentBranche seg5 = new SegmentBranche(4,8);
+		SegmentBranche seg6 = new SegmentBranche(4,9);
+		SegmentBranche seg7 = new SegmentBranche(5,10);
+		SegmentBranche seg8 = new SegmentBranche(6,9);
+		SegmentBranche seg9 = new SegmentBranche(7,9);
+		SegmentBranche seg10 = new SegmentBranche(7,10);
+		SegmentBranche seg11 = new SegmentBranche(7,11);
 		brancheGauche.ajouterSegment(seg1);
 		brancheGauche.ajouterSegment(seg2);
 		brancheGauche.ajouterSegment(seg3);
@@ -91,14 +91,14 @@ public class Grille {
 		setSegment(seg11);
 		
 		// Branche Droite
-		SegmentBranche seg12 = new SegmentBranche(11,10);
+		SegmentBranche seg12 = new SegmentBranche(10,11);
 		SegmentBranche seg13 = new SegmentBranche(11,11);
-		SegmentBranche seg14 = new SegmentBranche(10,12);
-		SegmentBranche seg15 = new SegmentBranche(9,13);
-		SegmentBranche seg16 = new SegmentBranche(8,13);
-		SegmentBranche seg17 = new SegmentBranche(8,14);
-		SegmentBranche seg18 = new SegmentBranche(8,15);
-		SegmentBranche seg19 = new SegmentBranche(9,16);
+		SegmentBranche seg14 = new SegmentBranche(12,10);
+		SegmentBranche seg15 = new SegmentBranche(13,9);
+		SegmentBranche seg16 = new SegmentBranche(13,8);
+		SegmentBranche seg17 = new SegmentBranche(14,8);
+		SegmentBranche seg18 = new SegmentBranche(15,8);
+		SegmentBranche seg19 = new SegmentBranche(16,9);
 		brancheDroite.ajouterSegment(seg12);
 		brancheDroite.ajouterSegment(seg13);
 		brancheDroite.ajouterSegment(seg14);
