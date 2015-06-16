c100209b1bdb0f9f30251e086a08bbbb8eff57cb
Clement SORANO <clement.sorano@etudiant.univ-lille1.fr>
2015-06-16 15:04:52 +0200

    lel

--- src/affichage/Plateau.java
+++ src/affichage/Plateau.java
@@ -2,7 +2,6 @@ package affichage;
 
 import grille.Grille;
 
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
---
+++ src/affichage/Tronc.java
@@ -0,0 +1,25 @@
+package affichage;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Tronc extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public Tronc(){
+		setPreferredSize(new Dimension(588, 340));
+	}
+	public void paint(Graphics g) {
+		Image img = new ImageIcon(getClass().getResource("/res/tronc.png"))
+				.getImage();
+		g.drawImage(img, 0, 0,null);
+	}
+
+}
